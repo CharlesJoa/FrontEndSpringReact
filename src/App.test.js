107725c67a8dfbedc93bb9705677e7092ecf0757
Mathias <naplings@hotmail.com>
2021-02-27 16:09:01 +0100

    update pakage.json for test

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,10 @@ import AppNavbar from "./AppNavbar";
 import BrowserRouter from "react-router-dom/BrowserRouter";
 
 test('renders the link of the nav bar', () => {
-  render(<BrowserRouter><AppNavbar/></BrowserRouter>);
-  const element = screen.getByText("Github for back");
-  expect(element).toBeInTheDocument();
+ //render(<BrowserRouter><AppNavbar/></BrowserRouter>);
+ // const element = screen.getByText("Github for back");
+  //expect(element).toBeInTheDocument();
+
+ render(<AppNavbar />);
+  expect(screen.getByText('Github for back')).toBeInTheDocument();
 });
